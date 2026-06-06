a68ef6b64048dafdafdbb4d0ed0f77d4415e7c35
Zippy <zippy@Zippys-Mac-mini.local>
2026-06-06 12:54:47 +0800

    Change default port to 8080 (avoid Grafana conflict)

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const APPS_DIR = path.join(__dirname, '../apps');
 
 // Serve static files for each app
